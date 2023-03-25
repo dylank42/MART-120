15b0a2d402c65873b1a0c981dfee18b770c205a3
dylan <dylan.kidd@umontana.edu>
2023-03-24 18:43:47 -0600

    Homework 9 Upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,34 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw(MU1)
+{
+    background(120,50,70);
+    fill(10,90,90);
+    circle(250,300,450);
+    fill(900);
+    circle(150,250,80);
+    circle(350,250,80);
+    fill(0);
+    square(150,250,20);
+    square(350,250,20);
+    fill(0,80,20);
+    rect(100,30,300,100);
+    line(100,120,450,120);
+    stroke(900);
+    fill(0);
+    ellipse(300,420,90,60);
+    fill(40,0,90);
+    triangle (240,300,250,350,300,300);
+    fill(90,90,0)
+    point(160,260)
+    point(360,260)
+    fill(600)
+    text ('Self Portrait',20,580)
+    textSize(20)
+    text ('dylan kidd',380,580)
+    textSize(50)
+    
+}
